68b7a89cd86191d5300ce122725155474acb0b0c
Ron <ron062394@gmail.com>
2023-12-19 04:04:49 +0800

    Updated Header and Home hero banner

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,16 @@ import { Link } from "react-router-dom";
 import './Header.css'
 
 function Header() {
+  
     return (
       <header className="header-section">
         <img className="logo" src="https://i.imgur.com/AwkQFdD.png" alt="roofing-logo" />
         <nav>
             <ul>
-                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/' >Home</Link></li>
                 <li><Link to='/products'>Products</Link></li>
                 <li><Link to='/about-us'>About Us</Link></li>
-                <li><Link to='/contact-us'>Get a Quote</Link></li>
+                <li className="contact-btn"><Link to='/contact-us'>Get a Quote</Link></li>
             </ul>
         </nav>
       </header>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,66 +1,30 @@
+import './Home.css'
+
 function Home() {
     return (
-      <section className="home-section">
-        <div className="hero-banner">
+      <main className="home-section">
+        <section className="hero-banner">
             <h1 className="hero-title">Discover the Perfect Roofing</h1>
             <p>Learn more</p>
-        </div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-        <div>new line</div>
-      </section>
+            <div className='info-cards'>
+              <span>
+                <h3>Openning hours:</h3>
+                <p>08:00am - 5:00pm</p>
+              </span>
+              <span>
+                <h3>Main Office:</h3>
+                <p>1658, Lorem Ipsilum, 3100</p>
+              </span>
+              <span>
+                <h3>Customer service:</h3>
+                <p>roofify@webmail.com</p>  
+              </span>
+            </div>
+        </section>
+        <section className='home-about-us'>
+          another content
+        </section>
+      </main>
     );
   }
   
