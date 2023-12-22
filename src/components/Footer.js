80a12a1ec57312f9b5baefeab3e2ec13ab813b10
Ron <ron062394@gmail.com>
2023-12-23 00:07:06 +0800

    Added changes on footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,21 @@
+import './Footer.css'
+
 function Footer() {
     return (
-      <footer className="footer-section">
-        <div>Contact Us:</div>
-        <div>Phone: (123) 456-7890</div>
+      <footer>
+        <div>
+          <div>Contact Us:</div>
+          <div>Phone: (123) 456-7890</div>
+          <div>Email:</div>
+          <div>info@roofingcompany.com</div>
+          <div>Address:</div>
+          <div>123 Main Street, City, State, Zip Code</div>
+          <div>© 2023 Your Roofing Company. All Rights Reserved.</div>
+        </div>
+        <div>
+          <div>Follow Us:</div>
+          <div>Facebook | Twitter | LinkedIn | Instagram</div>
+        </div>
       </footer>
     );
   }
