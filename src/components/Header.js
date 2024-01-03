e396997bda090870a6fb809b722a026f86c58517
Ron <ron062394@gmail.com>
2024-01-03 21:21:26 +0800

    Added responsiveness on navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,25 +2,22 @@ import { Link } from "react-router-dom";
 import './Header.css'
 
 function Header() {
-  
     return (
-      <header>
-        <div className="header-section">
-
-          <img className="logo" src="https://i.imgur.com/AwkQFdD.png" alt="roofing-logo" />
-          <nav>
-              <ul>
-                  <li><Link to='/' >Home</Link></li>
-                  <li><Link to='/products'>Products</Link></li>
-                  <li><Link to='/about-us'>About Us</Link></li>
-                  <li className="contact-btn"><Link to='/contact-us'>Get a Quote</Link></li>
-              </ul>
-          </nav>
-
-        </div>
-
-      </header>
+        <header>
+            <div className="header-section">
+                <img className="logo" src="https://i.imgur.com/AwkQFdD.png" alt="roofing-logo" />
+                <nav className="navbar">
+                    <ul className="nav-links">
+                        <li><Link to='/' >Home</Link></li>
+                        <li><Link to='/products'>Products</Link></li>
+                        <li><Link to='/about-us'>About Us</Link></li>
+                        <li className="contact-btn"><Link to='/contact-us'>Get a Quote</Link></li>
+                    </ul>
+                    <span className="menu-icon">click mey</span>
+                </nav>
+            </div>
+        </header>
     );
-  }
-  
-  export default Header;
+}
+
+export default Header;
