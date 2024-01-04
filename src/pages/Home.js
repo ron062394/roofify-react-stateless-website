ea03f7b98c82aa2ddad6d95fe2359fb1bd45238b
Ron <ron062394@gmail.com>
2024-01-04 22:33:17 +0800

    Added some changes on services

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,6 +19,7 @@ function Home() {
                 <p>roofify@webmail.com</p>  
               </span>
             </div>
+            
         </section>
         <section className='home-services'>
           <div className='our-services-container'>
