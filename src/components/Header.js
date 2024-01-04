9cc2ffed9506dc61665253206da0742f96c738ec
Ron <ron062394@gmail.com>
2024-01-04 22:47:29 +0800

    Added responsiveness on hero banner

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,11 @@ function Header() {
                         <li><Link to='/about-us'>About Us</Link></li>
                         <li className="contact-btn"><Link to='/contact-us'>Get a Quote</Link></li>
                     </ul>
-                    <span className="menu-icon">click mey</span>
+                    <span className="menu-icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 50 50">
+<path d="M 0 7.5 L 0 12.5 L 50 12.5 L 50 7.5 Z M 0 22.5 L 0 27.5 L 50 27.5 L 50 22.5 Z M 0 37.5 L 0 42.5 L 50 42.5 L 50 37.5 Z"></path>
+</svg>
+                    </span>
                 </nav>
             </div>
         </header>
