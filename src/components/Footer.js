615bee73495ff16482d8d031e931b83b8c06d5e2
Ron <ron062394@gmail.com>
2024-01-05 00:03:52 +0800

    Added some product page contents

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,6 +8,9 @@ function Footer() {
           <div>Phone: (123) 456-7890</div>
           <div>Email:</div>
           <div>info@roofingcompany.com</div>
+
+        </div>
+        <div>
           <div>Address:</div>
           <div>123 Main Street, City, State, Zip Code</div>
           <div>© 2023 Your Roofing Company. All Rights Reserved.</div>
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,10 +1,26 @@
 import './Products.css'
 function Products() {
     return (
-      <section className="products-section fade-in">
-        <div className="product-container">This is the products section</div>
+      <section className="products-section">
+        <div className='featured-product'>
+          <img className='featured-product-img' src="https://dimond-roofing.s3.amazonaws.com/gallery/_1200x630_crop_center-center_none/DD400-Kawarau-Falls-2.jpg" alt="" />
+        </div>
+        <div className="product-container">
+          <div className='product-card'>
+            <img src="https://i.imgur.com/KX1LDn5.png" alt="rib-roof-image" />
+            <div>
+              <h3>Rib Type</h3>  
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit nam eius veniam dolorem libero! Odit, non consectetur. Reprehenderit quod recusandae, vel voluptates magni aperiam animi reiciendis corporis est unde libero, et assumenda officiis sit perspiciatis, fuga provident quis quasi iste harum? Totam non, earum dolor hic praesentium quod? Pariatur, dignissimos.</p>
+            </div>            
+          </div>
+        </div>
       </section>
     );
   }
+  
+
+
+
+
   
   export default Products;
