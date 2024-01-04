b4d0cf7b55bef85bb27ab59f3b90ea8429351645
Ron <ron062394@gmail.com>
2024-01-05 06:07:36 +0800

    Added more grid products hover effect

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -2,31 +2,74 @@ import './Products.css'
 function Products() {
     return (
       <section className="products-section">
-        <div className='featured-product'>
-          <img className='featured-product-img' src="https://dimond-roofing.s3.amazonaws.com/gallery/_1200x630_crop_center-center_none/DD400-Kawarau-Falls-2.jpg" alt="" />
-        </div>
+        <img className="featured-product-img" src="https://5estimates.com/wp-content/uploads/2020/03/Metal-Roof-Color-Options.jpg" alt="featured-roof" />
         <div className="product-container">
           <div className='product-card'>
-            <img className='product-img' src="https://i.imgur.com/KX1LDn5.png" alt="rib-roof-image" />
+            <div className='product-specs'>
+              <img className='diameter' src="https://i.imgur.com/Yfbefkw.png" alt="diameter" />
+              <h3>Available colors</h3>
+              <p>Red, blue, green</p>
+            </div>
+            <img className='product-img' src="https://i.imgur.com/U2hETcF.png" alt="rib-roof-image" />
             <div>
-              <h3>Rib-Type</h3>  
+              <h2>Rib-type</h2>  
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit nam eius veniam dolorem libero! Odit, non consectetur. Reprehenderit quod recusandae, vel voluptates magni aperiam animi reiciendis corporis est unde libero, et assumenda officiis sit perspiciatis, fuga provident quis quasi iste harum? Totam non, earum dolor hic praesentium quod? Pariatur, dignissimos.</p>
             </div>            
           </div>
+
           <div className='product-card'>
+            <div className='product-specs'>
+              <img className='diameter' src="https://i.imgur.com/Yfbefkw.png" alt="diameter" />
+              <h3>Available colors</h3>
+              <p>Red, blue, green</p>
+            </div>
+            <img className='product-img' src="https://i.imgur.com/LLkXY0A.png" alt="rib-roof-image" />
+            <div>
+              <h2>Multi-Rib</h2>  
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit nam eius veniam dolorem libero! Odit, non consectetur. Reprehenderit quod recusandae, vel voluptates magni aperiam animi reiciendis corporis est unde libero, et assumenda officiis sit perspiciatis, fuga provident quis quasi iste harum? Totam non, earum dolor hic praesentium quod? Pariatur, dignissimos.</p>
+            </div>            
+          </div>
+
+          <div className='product-card'>
+            <div className='product-specs'>
+              <img className='diameter' src="https://i.imgur.com/Yfbefkw.png" alt="diameter" />
+              <h3>Available colors</h3>
+              <p>Red, blue, green</p>
+            </div>
+            <img className='product-img' src="https://i.imgur.com/XxM9Gbk.png" alt="rib-roof-image" />
+            <div>
+              <h2>Twin-Rib</h2>  
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit nam eius veniam dolorem libero! Odit, non consectetur. Reprehenderit quod recusandae, vel voluptates magni aperiam animi reiciendis corporis est unde libero, et assumenda officiis sit perspiciatis, fuga provident quis quasi iste harum? Totam non, earum dolor hic praesentium quod? Pariatur, dignissimos.</p>
+            </div>            
+          </div>
+
+          <div className='product-card'>
+            <div className='product-specs'>
+              <img className='diameter' src="https://i.imgur.com/Yfbefkw.png" alt="diameter" />
+              <h3>Available colors</h3>
+              <p>Red, blue, green</p>
+            </div>
             <img className='product-img' src="https://i.imgur.com/AqGwUvl.png" alt="rib-roof-image" />
             <div>
-              <h3>Corrugated</h3>  
+              <h2>Corrugated</h2>  
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit nam eius veniam dolorem libero! Odit, non consectetur. Reprehenderit quod recusandae, vel voluptates magni aperiam animi reiciendis corporis est unde libero, et assumenda officiis sit perspiciatis, fuga provident quis quasi iste harum? Totam non, earum dolor hic praesentium quod? Pariatur, dignissimos.</p>
             </div>            
           </div>
+
           <div className='product-card'>
-            <img className='product-img' src="https://i.imgur.com/KX1LDn5.png" alt="rib-roof-image" />
+            <div className='product-specs'>
+              <img className='diameter' src="https://i.imgur.com/Yfbefkw.png" alt="diameter" />
+              <h3>Available colors</h3>
+              <p>Red, blue, green</p>
+            </div>
+            <img className='product-img' src="https://i.imgur.com/AqGwUvl.png" alt="rib-roof-image" />
             <div>
-              <h3>Rib Type</h3>  
+              <h2>Tile-Span: Roman Style</h2>  
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit nam eius veniam dolorem libero! Odit, non consectetur. Reprehenderit quod recusandae, vel voluptates magni aperiam animi reiciendis corporis est unde libero, et assumenda officiis sit perspiciatis, fuga provident quis quasi iste harum? Totam non, earum dolor hic praesentium quod? Pariatur, dignissimos.</p>
             </div>            
           </div>
+
+      
         </div>
       </section>
     );
