05cf46065a55c978ff569dd0402689d8e84ecc4d
Ron <ron062394@gmail.com>
2024-01-07 00:32:49 +0800

    app.js minor changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
-
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import Products from "./pages/Products";
-import Contact from "./pages/Contact";
-import About from "./pages/About";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Contact from './pages/Contact';
+import About from './pages/About';
 
 function App() {
   return (
     <div className="App">
-        <Router>
-        <Header/>
-          <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/products' element={<Products/>}/>
-            <Route path='/about-us' element={<About/>}/>
-            <Route path='/contact-us' element={<Contact/>}/>
-          </Routes>
-          <Footer/>
-        </Router>
-        
-
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/about-us" element={<About />} />
+          <Route path="/contact-us" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
