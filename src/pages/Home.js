d3caefc487032add2fd3f7de8ce0ed1da866d284
Ron <ron062394@gmail.com>
2024-01-08 23:13:31 +0800

    changed project section design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -64,36 +64,58 @@ function Home() {
         </section>
         
         <section className='project-section'>
-          <div className='project-description'>
-            <h2>See Our Recent Projects</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem voluptatibus minus, sequi dolorum, dignissimos culpa asperiores incidunt provident earum, odio ratione dolor atque aliquam obcaecati ut itaque neque deleniti aliquid.</p>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis vitae illum totam laboriosam ipsa unde repudiandae impedit officiis obcaecati. Eveniet voluptates omnis adipisci maiores quibusdam nemo, odio error. Eaque at placeat adipisci esse maiores? Enim vero impedit dicta doloremque, odit earum quia praesentium, officia deserunt delectus placeat laboriosam voluptatibus. Ut odit veniam enim quisquam molestiae molestias fuga aliquid commodi vel, consequuntur hic dolorum distinctio dolores sequi itaque blanditiis.</p>
+          <div className='project-container'>
+            <div className='project-description'>
+              <span>SEE OUR LATEST PROJECTS</span>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem voluptatibus minus, sequi dolorum, dignissimos culpa asperiores incidunt provident earum, odio ratione dolor atque aliquam obcaecati ut itaque neque deleniti aliquid.</p>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis vitae illum totam laboriosam ipsa unde repudiandae impedit officiis obcaecati. Eveniet voluptates omnis adipisci maiores quibusdam nemo, odio error. Eaque at placeat adipisci esse maiores? Enim vero impedit dicta doloremque.</p>
+            </div>
+            <div  className='project-card-container'>
+              <div className="project-card">
+                <img className='project-img' src="https://res.cloudinary.com/devex/image/fetch/c_scale,f_auto,q_auto,w_720/https://lh3.googleusercontent.com/nLOn_99rh8piQprZEboC3bpe7yj80FRwoBLGtoxLdz6YuxKobErY3_N8xcb4khSaakjO70uCjj1T2gVxp8NvCxyC3U0u5mtskoCEjcIJE8inm_RSaRIEYwo8kDeBzuIoeTmqG9_1" alt="" />
+              </div>
+              
+              <div className="project-card">
+                  <img className='project-img' src="https://www.mydamus.com/wp-content/uploads/2023/04/File_004.jpg" alt="" />
+              </div>
+              
+              <div className="project-card">
+                <img className='project-img' src="https://readmetalroofing.com/wp-content/uploads/2023/01/Journey1.jpg" alt="" /> 
+              </div>
+
+              <div className="project-card">
+                <img className='project-img' src="https://scsroofing.co.uk/wp-content/uploads/2023/10/over-roofing-to-a-factory-roof-in-Littlehampton-West-Sussex-1024x768.jpg" alt="" /> 
+              </div>
+
+              <div className="project-card">
+                <img className='project-img' src="https://5.imimg.com/data5/SELLER/Default/2023/7/324588203/QP/DN/BQ/86817236/low-cost-factory-roofing-shed.jpg" alt="" /> 
+              </div>
+
+              <div className="project-card">
+                <img className='project-img' src="https://images.squarespace-cdn.com/content/v1/5937137fe58c629cc59102d1/1674833787303-ZSW5UFCR6PMFDESJVQFZ/Screen%2BShot%2B2022-08-25%2Bat%2B10.30.06%2BAM.jpg?format=1500w" alt="" />
+              </div>
+            </div>
           </div>
-          <div  className='project-container'>
-            <div className="project-card">
-              <img className='project-img' src="https://res.cloudinary.com/devex/image/fetch/c_scale,f_auto,q_auto,w_720/https://lh3.googleusercontent.com/nLOn_99rh8piQprZEboC3bpe7yj80FRwoBLGtoxLdz6YuxKobErY3_N8xcb4khSaakjO70uCjj1T2gVxp8NvCxyC3U0u5mtskoCEjcIJE8inm_RSaRIEYwo8kDeBzuIoeTmqG9_1" alt="" />
+        <div  className='home-experience'>
+          <div>
+              <span className='big-numbers'>10</span>
+              <span>YEARS OF EXPERIENCE</span> 
             </div>
-            
-            <div className="project-card">
-                <img className='project-img' src="https://www.mydamus.com/wp-content/uploads/2023/04/File_004.jpg" alt="" />
+            <div>
+              <span  className='big-numbers'>28</span>
+              <span>ROOFING EXPERTS</span> 
             </div>
-            
-            <div className="project-card">
-              <img className='project-img' src="https://readmetalroofing.com/wp-content/uploads/2023/01/Journey1.jpg" alt="" /> 
+            <div>
+              <span  className='big-numbers'>500+</span>
+              <span>COMPLETED PROJECTS</span> 
             </div>
-
-            <div className="project-card">
-              <img className='project-img' src="https://scsroofing.co.uk/wp-content/uploads/2023/10/over-roofing-to-a-factory-roof-in-Littlehampton-West-Sussex-1024x768.jpg" alt="" /> 
-            </div>
-
-            <div className="project-card">
-              <img className='project-img' src="https://5.imimg.com/data5/SELLER/Default/2023/7/324588203/QP/DN/BQ/86817236/low-cost-factory-roofing-shed.jpg" alt="" /> 
+            <div>
+              <span  className='big-numbers'>6</span>
+              <span>SATELITE BRANCHES</span> 
             </div>
 
-            <div className="project-card">
-              <img className='project-img' src="https://images.squarespace-cdn.com/content/v1/5937137fe58c629cc59102d1/1674833787303-ZSW5UFCR6PMFDESJVQFZ/Screen%2BShot%2B2022-08-25%2Bat%2B10.30.06%2BAM.jpg?format=1500w" alt="" />
-            </div>
           </div>
+
         </section>
 
         <section className='order-section'>
@@ -103,7 +125,7 @@ function Home() {
               <h3>Free Consultation</h3>
               <p>Expert installations tailored to your needs. We use high-quality materials and ensure precision in every project to provide durable and beautiful roofs.</p>
             </div>
-            <div className='order-card'>
+            <div className='order-card order'>
               <img src="https://i.imgur.com/Ve7l9P0.png" alt="" className='services-icon'/>
               <h3>Order</h3>
               <p>Expert Roofing Services Tailored to Your Needs</p>
@@ -113,6 +135,7 @@ function Home() {
               <h3>Pick-up / Deliver</h3>
               <p>Comprehensive roof inspections to identify potential issues early. Our detailed assessments help maintain the integrity of your roof and prevent future problems.</p>
             </div>
+            
           </div>
         </section>
       </main>
