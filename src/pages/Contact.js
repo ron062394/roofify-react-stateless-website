0e997d6d59ef723432af02f7de08a2954f612dd1
Ron <ron062394@gmail.com>
2024-01-20 22:08:53 +0800

    added changes on contact me page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -15,17 +15,17 @@ function Contact() {
 
             <div className='contact-us-cards-container'>
               <div className='contact-us-cards'>
-                <img className='contact-icon' src="https://cdn-icons-png.flaticon.com/512/6395/6395240.png" alt="" />
+                <img className='contact-icon' src="https://pngimg.com/d/google_maps_pin_PNG4.png" alt="" />
                 <div>Visit us</div>
                 <div>123 Main Street, City, State, ZIP</div>
               </div>
               <div className='contact-us-cards'>
-                <img className='contact-icon' src="https://icon-library.com/images/phone-call-icon-png/phone-call-icon-png-10.jpg" alt="" />
+                <img className='contact-icon' src="https://cdn-icons-png.flaticon.com/512/6730/6730822.png" alt="" />
                 <div>Call us</div>
                 <div> (123) 456-78900</div>
               </div>
               <div className='contact-us-cards'>
-                <img className='contact-icon' src="https://cdn-icons-png.flaticon.com/512/6395/6395240.png" alt="" />
+                <img className='contact-icon' src="https://i.pinimg.com/originals/b3/02/51/b302515a99b3e4a94ed563c6bbb93075.png" alt="" />
                 <div>Email us</div>
                 <div> Email: sales@roofingcompany.com</div>
               </div>
@@ -39,24 +39,35 @@ function Contact() {
           <p>Phone: (123) 456-78909</p>
           <p>Email: info@roofingcompany.com</p>
         </div>
-
         <div class="satellite-offices">
-          <h2>Satellite Offices</h2>
-          <p>Satellite Office 1 Address: 456 Satellite Ave, City, State, ZIP</p>
-          <p>Email: satellite1@example.com</p>
-          <p>Phone: +987654321</p>
 
-          <p>Satellite Office 2 Address: 789 Rooftop Blvd, City, State, ZIP</p>
-          <p>Email: satellite2@example.com</p>
-          <p>Phone: +111222333</p>
+          <h2>Satellite Offices</h2>          
 
-          <p>Satellite Office 3 Address: 789 Rooftop Blvd, City, State, ZIP</p>
-          <p>Email: satellite3@example.com</p>
-          <p>Phone: +111222333</p>
+          <div>
+            <p>Satellite Office 1 Address: 456 Satellite Ave, City, State, ZIP</p>
+            <p>Email: satellite1@example.com</p>
+            <p>Phone: +987654321</p>
+          </div>
 
-          <p>Satellite Office 4 Address: 789 Rooftop Blvd, City, State, ZIP</p>
-          <p>Email: satellite4@example.com</p>
-          <p>Phone: +111222333</p>
+          <div>
+            <p>Satellite Office 2 Address: 789 Rooftop Blvd, City, State, ZIP</p>
+            <p>Email: satellite2@example.com</p>
+            <p>Phone: +111222333</p>
+          </div>
+
+          <div>
+            <p>Satellite Office 3 Address: 789 Rooftop Blvd, City, State, ZIP</p>
+            <p>Email: satellite3@example.com</p>
+            <p>Phone: +111222333</p>
+          </div>
+
+          <div>
+            <p>Satellite Office 4 Address: 789 Rooftop Blvd, City, State, ZIP</p>
+            <p>Email: satellite4@example.com</p>
+            <p>Phone: +111222333</p>
+          </div>
+
+
 
         </div>
       </main>
